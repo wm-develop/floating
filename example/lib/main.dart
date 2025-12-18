77e2361fc6b5136073a3eb51019d2b7e534053a3
qshh <3218696007@qq.com>
2025-12-18 23:38:55 +0800

    配置鸿蒙项目

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,94 +1,94 @@
-// import 'dart:math';
+import 'dart:math';
 
-// import 'package:flutter/material.dart';
-// import 'dart:async';
+import 'package:flutter/material.dart';
+import 'dart:async';
 
-// import 'package:floating/floating.dart';
+import 'package:floating/floating.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() {
+  runApp(MyApp());
+}
 
-// class MyApp extends StatefulWidget {
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-// class _MyAppState extends State<MyApp> {
-//   final floating = Floating();
+class _MyAppState extends State<MyApp> {
+  final floating = Floating();
 
-//   @override
-//   void dispose() {
-//     floating.dispose();
-//     super.dispose();
-//   }
+  @override
+  void dispose() {
+    // floating.dispose();
+    super.dispose();
+  }
 
-//   Future<void> enablePip(
-//     BuildContext context, {
-//     bool autoEnable = false,
-//   }) async {
-//     final rational = Rational.landscape();
-//     final screenSize =
-//         MediaQuery.of(context).size * MediaQuery.of(context).devicePixelRatio;
-//     final height = screenSize.width ~/ rational.aspectRatio;
+  Future<void> enablePip(
+    BuildContext context, {
+    bool autoEnable = false,
+  }) async {
+    final rational = Rational.landscape();
+    final screenSize =
+        MediaQuery.of(context).size * MediaQuery.of(context).devicePixelRatio;
+    final height = screenSize.width ~/ rational.aspectRatio;
 
-//     final arguments = autoEnable
-//         ? AutoEnable(
-//             aspectRatio: rational,
-//             sourceRectHint: Rectangle<int>(
-//               0,
-//               (screenSize.height ~/ 2) - (height ~/ 2),
-//               screenSize.width.toInt(),
-//               height,
-//             ),
-//           )
-//         : EnableManual(
-//             aspectRatio: rational,
-//             sourceRectHint: Rectangle<int>(
-//               0,
-//               (screenSize.height ~/ 2) - (height ~/ 2),
-//               screenSize.width.toInt(),
-//               height,
-//             ),
-//           );
+    final arguments = autoEnable
+        ? AutoEnable(
+            aspectRatio: rational,
+            sourceRectHint: Rectangle<int>(
+              0,
+              (screenSize.height ~/ 2) - (height ~/ 2),
+              screenSize.width.toInt(),
+              height,
+            ),
+          )
+        : EnableManual(
+            aspectRatio: rational,
+            sourceRectHint: Rectangle<int>(
+              0,
+              (screenSize.height ~/ 2) - (height ~/ 2),
+              screenSize.width.toInt(),
+              height,
+            ),
+          );
 
-//     final status = await floating.enable(arguments);
-//     debugPrint('PiP enabled? $status');
-//   }
+    final status = await floating.enable(arguments);
+    debugPrint('PiP enabled? $status');
+  }
 
-//   @override
-//   Widget build(BuildContext context) => MaterialApp(
-//         theme: ThemeData.dark(),
-//         home: PiPSwitcher(
-//           childWhenDisabled: Scaffold(
-//             body: Center(child: Image.asset('assets/image.jpg')),
-//             floatingActionButtonLocation:
-//                 FloatingActionButtonLocation.centerFloat,
-//             floatingActionButton: FutureBuilder<bool>(
-//               future: floating.isPipAvailable,
-//               initialData: false,
-//               builder: (context, snapshot) => snapshot.data ?? false
-//                   ? Column(
-//                       children: [
-//                         FloatingActionButton.extended(
-//                           onPressed: () => enablePip(context),
-//                           label: const Text('Enable PiP'),
-//                           icon: const Icon(Icons.picture_in_picture),
-//                         ),
-//                         const SizedBox(height: 12),
-//                         FloatingActionButton.extended(
-//                           onPressed: () => enablePip(context, autoEnable: true),
-//                           label: const Text('Enable PiP on app minimize'),
-//                           icon: const Icon(Icons.auto_awesome),
-//                         ),
-//                       ],
-//                     )
-//                   : const Card(
-//                       child: Text('PiP unavailable'),
-//                     ),
-//             ),
-//           ),
-//           childWhenEnabled: Image.asset('assets/image.jpg'),
-//         ),
-//       );
-// }
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        theme: ThemeData.dark(),
+        // home: PiPSwitcher(
+        //   childWhenDisabled: Scaffold(
+        //     body: Center(child: Image.asset('assets/image.jpg')),
+        //     floatingActionButtonLocation:
+        //         FloatingActionButtonLocation.centerFloat,
+        //     floatingActionButton: FutureBuilder<bool>(
+        //       future: floating.isPipAvailable,
+        //       initialData: false,
+        //       builder: (context, snapshot) => snapshot.data ?? false
+        //           ? Column(
+        //               children: [
+        //                 FloatingActionButton.extended(
+        //                   onPressed: () => enablePip(context),
+        //                   label: const Text('Enable PiP'),
+        //                   icon: const Icon(Icons.picture_in_picture),
+        //                 ),
+        //                 const SizedBox(height: 12),
+        //                 FloatingActionButton.extended(
+        //                   onPressed: () => enablePip(context, autoEnable: true),
+        //                   label: const Text('Enable PiP on app minimize'),
+        //                   icon: const Icon(Icons.auto_awesome),
+        //                 ),
+        //               ],
+        //             )
+        //           : const Card(
+        //               child: Text('PiP unavailable'),
+        //             ),
+        //     ),
+        //   ),
+        //   childWhenEnabled: Image.asset('assets/image.jpg'),
+        // ),
+      );
+}
